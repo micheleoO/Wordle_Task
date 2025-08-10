0d25e855a492130032bd69c87c556707c64173b8
Michele <miichellemansour@gmail.com>
2025-08-10 22:42:27 +0300

    Uploading words _ Word selection

--- Game.py
+++ Game.py
@@ -1,1 +1,12 @@
-print("  I'm having a lot of fun :)  ")
+# milestone_01.py
+import random, os
+
+def load_words(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        words = [w.strip() for w in f if len(w.strip())==5]
+    return words
+
+if __name__ == "__main__":
+    path = os.path.join(os.path.dirname(__file__), "words.txt")
+    words = load_words(path)
+    print(f"Loaded {len(words)} words. Example random word: {random.choice(words).upper()}")
